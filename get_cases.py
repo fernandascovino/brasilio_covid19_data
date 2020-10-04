6d854c3cbb0c75f4079db63f2b107b8e580b5c18
Fernanda Scovino <fscovinom@gmail.com>
2020-10-04 02:31:41 -0300

    add null text

--- get_cases.py
+++ get_cases.py
@@ -1,6 +1,7 @@
 import yaml
 from loguru import logger
 import rj_script, pr_script
+import pandas as pd
 
 
 @logger.catch()
@@ -15,6 +16,10 @@ def collect_state_report(date, uf):
     if uf == "RJ":
         df = rj_script.main(date, config, uf)
         path = "outputs/rj/rj_{}.xlsx".format(date[3:] + "_" + date[:2])
+
+    if not isinstance(df, pd.DataFrame):
+        logger.error("Output não é um dataframe!")
+        return
 
     df.to_excel(path)
     logger.success("Dados salvos em: {display}", display=path)
